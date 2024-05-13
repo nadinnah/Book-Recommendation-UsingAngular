ef9e367a4b7fcaed54b790bb3d5ad4fa8fa71aaf
AbdulrahmanMaged <abdulrahmanmaged@yahoo.com>
2024-05-13 11:23:57 +0300

    Modified login and add books page format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { BookPageComponent } from './book-page/book-page.component';
 import { BooksPageComponent } from './books-page/books-page.component';
+import { DbAdderComponent } from './db-adder/db-adder.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {path:'home-page', component:HomeComponent},
@@ -10,6 +11,7 @@ const routes: Routes = [
   {path:'books-page/tag/:tag', component:BooksPageComponent},
   {path:'book/:id', component:BookPageComponent},
   {path:'books-page',component:BooksPageComponent},
+  {path:'db-adder',component:DbAdderComponent},
   {path: 'auth/login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,8 +22,9 @@ import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     FontAwesomeModule,
     MatFormFieldModule,MatCardModule,MatInputModule,MatButtonModule,MatSelectModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync('noop')
